b839523b3de221b148e4e2ac7483a332edfcadec
ngalkina <ngalkina86@mail.ru>
2018-09-23 16:02:26 +0300

    исправление наименования методов

--- test_add_group.py
+++ test_add_group.py
@@ -16,7 +16,6 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost:441/addressbook/")
-        wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()

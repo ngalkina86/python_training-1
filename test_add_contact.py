b5d83f3bb636e73fb20a629bdf49b4c445dc950a
ngalkina <ngalkina86@mail.ru>
2018-09-24 16:04:08 +0300

    выделение самостоятельных методов

--- test_add_contact.py
+++ test_add_contact.py
@@ -10,15 +10,19 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost:441/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys('admin')
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.creat_new_contact(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def creat_new_contact(self, wd):
         # add new contact
         wd.find_element_by_link_text("add new").click()
         # fill contact firm
@@ -98,10 +102,17 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys("gfd")
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        # return to home page
-        wd.find_element_by_link_text("home page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys('admin')
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost:441/addressbook/")
 
     def tearDown(self):
         self.wd.quit()

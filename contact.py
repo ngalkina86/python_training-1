ca3f9080655a1e814abbae6a1c601b1ce0cb07b7
ngalkina <ngalkina86@mail.ru>
2018-09-27 15:16:34 +0300

    Изменение вспомогательного метода

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+
+    def __init__(self,firstname,middlename, lastname,nickname, title, company_name, address, home, mobile, phone_work,
+                 fax,email, email_12, email_13, homepage, bday, bmonth,byear, aday, amonth, ayear, address_2, phone_2,
+                 notes):
+        self. firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company_name = company_name
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.phone_work = phone_work
+        self.fax = fax
+        self.email = email
+        self.email_12 = email_12
+        self.email_13 = email_13
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address_2= address_2
+        self.phone_2= phone_2
+        self.notes = notes
--- test_add_group.py
+++ test_add_group.py
@@ -4,7 +4,6 @@ from group import Group
 from appolication import Application
 
 
-
 @pytest.fixture
 def app (request):
     fixture = Application ()

f7a68b8ea9b23ccc2ceb1b9adc54e98c4f3b2833
ngalkina <ngalkina86@mail.ru>
2018-09-29 16:48:22 +0300

    Создала новый тест для удаления группы, сделала общую для всех тестов фикстуру.

--- fixture/group.py
+++ fixture/group.py
@@ -7,6 +7,7 @@ class GroupHelper:
             wd = self.app.wd
             wd.find_element_by_link_text("group page").click()
 
+
     def create(self, group):
             wd = self.app.wd
             self.open_group_page()
@@ -25,6 +26,16 @@ class GroupHelper:
             # submit group creation
             wd.find_element_by_name("submit").click()
             self.return_to_group_page()
+
+    def delete_first_group(self):
+            wd = self.app.wd
+            self.open_group_page()
+            # select first group
+            wd.find_element_by_name("selected[]").click()
+            # submit deletion
+            wd.find_element_by_name("delete").click()
+            self.return_to_group_page()
+
 
     def open_group_page(self):
             wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app (request):
-    fixture = Application ()
-    request.addfinalizer (fixture. destroy)
-    return fixture
 
 
 def test_add_group(app):
@@ -19,5 +10,5 @@ def test_add_group(app):
 
 def test_add_empty_group(app):
     app.session.login(username = "admin", password = "secret")
-    app.group.create(Group(name ="", header ="", footer =""))
+    app.group.create(Group(name ="", header ="",footer =""))
     app.session.logout()

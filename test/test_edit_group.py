1873d3700df6e5f528182e1ffe3e16d84defa477
ngalkina <ngalkina86@mail.ru>
2018-10-11 23:49:47 +0300

    Доработка тестов: add, edit and modify group.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,7 +3,11 @@ from model.group import Group
 
 def test_edit_group(app):
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="Test group"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 

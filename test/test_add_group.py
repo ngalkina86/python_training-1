3a8a04ef5f24da64ee17fd2b0f99cbac870c9f79
ngalkina <ngalkina86@mail.ru>
2018-10-27 23:58:54 +0300

    загрузка тестовых данных из файлов (группы)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,8 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):
-    group = data_groups
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     group = Group(name ="test1", header ="reew", footer ="fdsf")
     app.group.create(group)

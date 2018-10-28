80e38f95146ef9d6b8c25a018c7fbf1b239ed8b8
ngalkina <ngalkina86@mail.ru>
2018-10-28 21:43:51 +0300

    загрузка тестовых данных из файлов (modify)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,8 @@
 from model.group import Group
 from random import randrange
 
-def test_modify_group_name(app):
+def test_modify_group_name(app,json_groups):
+    group = json_groups
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
